da3a5527509eb36d6e10674d7fd7459468aa9dc6
Barbara Mayfield <bmayfield1991@gmail.com>
2020-07-27 20:32:27 -0500

    work: about div with lorem ipsum paragraph blocks

---
+++ src/components/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="About">
+            <h2>About this Algorithm &gt;</h2>
+            <p>
+                I'm baby ennui chambray kogi everyday carry twee four dollar
+                toast deep v activated charcoal flannel. DIY farm-to-table
+                occupy, slow-carb succulents trust fund brooklyn chillwave. YOLO
+                church-key iPhone pop-up tousled put a bird on it, shaman paleo
+                typewriter air plant jianbing blue bottle art party raw denim.
+                Meggings vice cray locavore crucifix snackwave typewriter yuccie
+                kombucha authentic jianbing. Literally whatever quinoa locavore
+                knausgaard celiac readymade kickstarter hot chicken adaptogen
+                etsy austin.
+            </p>
+
+            <p>
+                Lyft VHS raclette, single-origin coffee fingerstache microdosing
+                humblebrag yuccie meggings neutra activated charcoal. Four loko
+                tattooed paleo succulents wolf chartreuse air plant occupy
+                forage kitsch palo santo blue bottle. Microdosing scenester
+                authentic flexitarian, ramps pug fingerstache shabby chic kogi
+                blog cliche fashion axe helvetica forage photo booth. Banjo
+                meggings bushwick gluten-free. Before they sold out irony small
+                batch cold-pressed, viral organic chicharrones lumbersexual
+                cornhole pour-over mumblecore pickled pitchfork crucifix. Woke
+                retro blog gastropub enamel pin. Poke before they sold out
+                jianbing lo-fi iceland, hella umami photo booth 8-bit.
+            </p>
+
+            <p>
+                3 wolf moon truffaut deep v, synth 90's marfa normcore yr
+                microdosing. Activated charcoal church-key drinking vinegar,
+                salvia hell of palo santo wolf retro flannel vegan food truck
+                deep v quinoa pickled literally. Tumblr organic kitsch cardigan
+                ramps tbh. Church-key green juice raw denim truffaut tattooed
+                hammock. Taxidermy cardigan chambray glossier kogi 90's
+                flexitarian chillwave cronut pok pok shoreditch ethical
+                skateboard retro post-ironic. Mustache microdosing ethical blue
+                bottle tousled cred. Squid pok pok gluten-free, helvetica palo
+                santo photo booth forage kitsch yr tilde.
+            </p>
+        </div>
+    );
+}
